6cabb895ef79edf93408ed4cc1c3d8b4e838adec
YaroslavBoliachevets <boliachevets.ya@gmail.com>
2022-09-26 17:31:51 +0300

    изменил сеттеры

--- src/App.js
+++ src/App.js
@@ -29,13 +29,13 @@ function App() {
     const name = event.currentTarget.name;
     switch (name) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevState => prevState + 1);
         break;
       case "neutral":
-        setNeutral(neutral + 1);
+        setNeutral(prevState => prevState + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevState => prevState + 1);
         break;
       default:
         return;
